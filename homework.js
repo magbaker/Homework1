e3b831a89fddf374851d3d96bbd54c259214bea8
Maggie Baker <mbaker@coalmarch.com>
2017-06-11 21:39:43 -0400

    Shopping Basket is working- can't figure out tabs yet though.

--- homework.js
+++ homework.js
@@ -1,15 +1,37 @@
-
-
+// Buy Now Button
 $('#BuyNowButton').click(function() {
-    var self = $(this);
-    if(self.hasClass('#BuyNowButton')){
-        self.text('BUY NOW!');
-      } else {
-        self.text('Added to Cart');
-        setTimeout(function() {
-            self.text('BUY NOW!').data('Item Added to Cart');
-        }, 2000);
-    }
-    $('.counter').html(function(i, val) {
-      return +val+1 });
+  var self = $(this);
+  if (self.hasClass('#BuyNowButton')) {
+    self.text('BUY NOW!');
+  } else {
+    self.text('Added to Cart');
+    setTimeout(function() {
+      self.text('BUY NOW!').data('Item Added to Cart');
+    }, 500);
+  }
+  $('.counter').html(function(i, val) {
+    return +val + 1
+  });
 });
+
+// Tabs
+// $('ul.Tabs').each(function(){
+//
+//   var $active, $content, $links = $(this).find('a');
+//   $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+//   $active.addClass('active');
+//   $content = $($active[0].hash);
+//   $links.not($active).each(function () {
+//     $(this.hash).hide();
+//   });
+//
+//   $(this).on('click', 'a', function(e){
+//     $active.removeClass('active');
+//     $content.hide();
+//     $active = $(this);
+//     $content = $(this.hash);
+//     $active.addClass('active');
+//     $content.show();
+//     e.preventDefault();
+//   });
+// });
